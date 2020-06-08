32ca4bf564a608f4a1e118ea24eece9e0114b4f7
Giorgia-M <62333057+Giorgia-M@users.noreply.github.com>
2020-06-08 12:49:28 +0200

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -779,11 +779,60 @@ plot(snow.multitemp.italy, col=clb, zlim=c(20,200))
 # boxplot
 boxplot(snow.multitemp.italy, horizontal=T,outline=F)
 
-    
+#################
+#################
+#################
 
+# 12. Species distribution modelling
 
+install.packages("sdm")
+library (sdm)
+library (raster)
+library (rgdal)
 
+file <- system.file("external/species.shp", package="sdm")
+species <- shapefile(file)
+species
+species$Occurrence
+plot(species)
 
-   
+plot(species[species$Occurrence == 1,],col='blue',pch=16)
+points(species[species$Occurrence == 0,],col='red',pch=16)
+
+# environmental variables
+path <- system.file("external", package="sdm") 
+lst <- list.files(path=path,pattern='asc$',full.names = T) #
+lst
+
+preds <- stack(lst)
+
+cl <- colorRampPalette(c('yellow','orange','red')) (100)
+plot(preds, col=cl)
+
+plot(preds$elevation, col=cl)
+points(species[species$Occurrence == 1,], pch=16)
+
+plot(preds$temperature, col=cl)
+points(species[species$Occurrence == 1,], pch=16)
+
+plot(preds$precipitation, col=cl)
+points(species[species$Occurrence == 1,], pch=16)
+
+plot(preds$vegetation, col=cl)
+points(species[species$Occurrence == 1,], pch=16)
+
+# model
+d <- sdmDAta(train=species, predictors=preds)
+d
+
+m1 <- sdm(Occurrence ~ elevation + precipitation + temperature + vegetation, data=d, methods='glm')
+p1 <- predict(m1, newdata=preds)
+plot(p1, col=cl)
+points(species[species$Occurrence == 1,], pch=16)
  
+##################################à
+
+# Exam project
+
+
 
